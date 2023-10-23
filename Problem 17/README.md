e9c958a7aa8730c35a70e2329255147d7c4b81d1
Gyanendu Bikash Boitai <gyanenduboitai2004@gmail.com>
2023-10-23 08:14:41 +0530

    Add problem 17 along with solution

---
+++ Problem 17/README.md
@@ -0,0 +1,13 @@
+# PROBLEM STATEMENT
+
+Write A Python Program To Get 4 Numbers From User And use Them In The Following Equation:
+d+a+2ab/d(4c+10)
+
+
+## EXPECTED OUTPUT
+        Enter a number: 2
+        Enter second number: 5
+        Enter third number: 4
+        Enter forth number: 6
+
+        The solution of the equation 'd+a+2*a*b/d*(4*c+10)' is: 94.6667
--- Problem 17/solution.py
+++ Problem 17/solution.py
@@ -1,1 +1,21 @@
 # Function to accept input from the user
+def readval():
+    a = float(input("\tEnter a number: "))
+    b = float(input("\tEnter second number: "))
+    c = float(input("\tEnter third number: "))
+    d = float(input("\tEnter forth number: "))
+    return a,b,c,d
+
+# Function to perfrom the operation
+def equation(a,b,c,d):
+    val = d+a+2*a*b/d*(4*c+10)
+    return val
+
+# Function to display the result
+def dispval(val):
+    print("\n\tThe solution of the equation 'd+a+2*a*b/d*(4*c+10)' is: {}".format(round(val,4)))
+
+# Mian program
+val = readval()
+res = equation(val[0],val[1],val[2],val[3])
+dispval(res)
