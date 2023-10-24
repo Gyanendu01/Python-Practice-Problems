dc00869d169daac6a1bf025c11064ff2115bc0ba
Gyanendu Bikash Boitai <gyanenduboitai2004@gmail.com>
2023-10-24 19:49:24 +0530

    Add problem 26 along with solution

---
+++ Problem 26/README.md
@@ -0,0 +1,29 @@
+# PROBLEM STATEMENT
+
+Write A Python Program To Get Number To Check, Whether It Is Positive, Negative Or Equal To Zero 
+
+## EXPECTED OUTPUT
+
+### OUTPUT 
+        Enter a number: 22
+
+        Number is positive
+
+        Do you want to check another number? (y/n): y
+        Enter a number: 0
+
+        Number is zero
+
+        Do you want to check another number? (y/n): y
+        Enter a number: -2
+
+        Number is negative
+
+        Do you want to check another number? (y/n): y
+        Enter a number: 1980
+
+        Number is positive
+
+        Do you want to check another number? (y/n): n
+
+        Thank you!
---
+++ Problem 26/solution.py
@@ -0,0 +1,27 @@
+# Function to accept a number from the user
+def usr_input():
+    num = float(input("\tEnter a number: "))
+    return num
+
+# Function to check whether the number is +ve -ve or 0
+def check_num(num):
+    if num > 0:
+        print("\n\tNumber is positive")
+    elif num < 0:
+        print("\n\tNumber is negative")
+    elif num == 0:
+        print("\n\tNumber is zero")
+
+# Main program
+num = usr_input()
+check_num(num)
+
+while True:
+    condn = input("\n\tDo you want to check another number? (y/n): ")
+    condn = condn.lower()
+    if condn == 'y':
+        num = usr_input()
+        check_num(num)
+    else:
+        print("\n\tThank you!")
+        break
