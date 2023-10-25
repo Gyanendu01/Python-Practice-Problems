51c9051bde2d72290ec504eada9b66cd3f8cb17c
Gyanendu Bikash Boitai <gyanenduboitai2004@gmail.com>
2023-10-25 22:34:03 +0530

    Add problem 33 along with solution

---
+++ Problem 33/README.md
@@ -0,0 +1,7 @@
+# PROBLEM STATEMENT
+Write A Python Program To Convert Degree To Radian 
+
+
+## OUTPUT
+        Enter the angle in degrees: 34
+        34 degrees is equal to 0.5934119456780721 radians
---
+++ Problem 33/solution.py
@@ -0,0 +1,10 @@
+import math
+
+# Input angle in degrees
+degrees = int(input("\tEnter the angle in degrees: "))
+
+# Convert degrees to radians
+radians = math.radians(degrees)
+
+# Print the result
+print(f"\t{degrees} degrees is equal to {radians} radians")
