f525891091470afad7c25c9af046a80d5df6deae
Gyanendu Bikash Boitai <gyanenduboitai2004@gmail.com>
2023-10-31 20:23:39 +0530

    Add problem 44 along with solution

---
+++ problem 44/README.md
@@ -0,0 +1,8 @@
+# PROBLEM STATEMENT
+
+
+Write A Python Program To Find Area Of Triangle And Then Get A Number From The User, Find Square Of That Number, Then Add That With Area Of The Triangle. Display Result To User
+
+
+## OUTPUT
+        
---
+++ problem 44/solution.py
@@ -0,0 +1,10 @@
+# Function to take user input
+def user_input():
+    len = float(input("\n\tEnter the length of the traingle: "))
+    bred = float(input("\tEnter the breadth of the traingle: "))
+    return len,bred
+
+# Function to calculate the area of the traingle
+def area_of_traingle(len,bred):
+    ar = 0.5*len*bred
+    print("\n\tArea of the traingle is {}".format(ar))
