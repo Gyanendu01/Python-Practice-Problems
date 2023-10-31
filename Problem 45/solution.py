4a1d7f2b3aa749522dafa7c4bef012a7ffe78c1c
Gyanendu Bikash Boitai <gyanenduboitai2004@gmail.com>
2023-10-31 20:50:26 +0530

    update solution

--- Problem 45/solution.py
+++ Problem 45/solution.py
@@ -3,9 +3,17 @@ def get_dates():
     date1 = input("\n\tEnter first date(day/month/year): ")
     date2 = input("\n\tEnter second date(day/month/year): ")
     
-    date1 = date1.split("/")
+    date1 = date1.split("/",".")
     date1 = int(date1[0])
     
-    date1 = date1.split("/")
-    date1 = int(date1[0])
-    
+    date2 = date2.split("/")
+    date2 = int(date2[0])
+
+    return date1, date2
+
+# function to find hours between two dates
+def get_hours(d1,d2):
+    if d1 == d2:
+        print("\n\tNumber of hours between two dates: 24 Hours")
+    if d1 > d2:
+        print("\n\tNumber of hours between two dates: {}".format6((d1-d2)*24))
