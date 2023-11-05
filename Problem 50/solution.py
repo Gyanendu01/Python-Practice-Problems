dbeb2750e08389c871f9c0039b414da918fff6ec
Gyanendu Bikash Boitai <gyanenduboitai2004@gmail.com>
2023-11-05 09:13:10 +0530

    update solution of problem 50

--- Problem 50/solution.py
+++ Problem 50/solution.py
@@ -1,7 +1,12 @@
 # function to get input from user
 def usr_input():
     num = int(input("\n\tEnter a number: "))
-    percentage = int(input("\tEnter The percentage: "))
+    while True:
+        percentage = int(input("\tEnter The percentage: "))
+        if percentage >0 and percentage <=100:
+            break
+        else:
+            print("\n\tplease enter a number between 0 and 100")
     return num, percentage
 
 # function to calculate percentage
@@ -11,3 +16,16 @@ def cal_percentage(num,perc):
 # main program
 val = usr_input()
 cal_percentage(val[0],val[1])
+
+while True:
+    while True:
+        condn = input("\n\tDo you want to continue (y/n): ")
+        condn = condn.lower()
+        if condn in ['y', 'n']:
+            break
+    if condn == 'y':
+        val = usr_input()
+        cal_percentage(val[0],val[1])
+    else:
+        print("\n\tThank You")
+        break
