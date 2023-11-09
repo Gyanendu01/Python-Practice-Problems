7686e9629d69acf7c27b2f48598f59321f311906
Gyanendu Bikash Boitai <gyanenduboitai2004@gmail.com>
2023-11-09 21:12:22 +0530

    Add problem 57 along with solution

---
+++ Problem 57/README.md
@@ -0,0 +1,3 @@
+# PROBLEM STATEMENT
+
+Write a python program to get student information like name, father's name, age, date-of-birth, contact details and display information in side by side format.
---
+++ Problem 57/solution.py
@@ -0,0 +1,23 @@
+# function to accpet the user details
+def get_user_details():
+    d = {
+        'name': input("\n\tEnter your name: "),
+        'father_name': input("\n\tEnter your father's name: "),
+        'age': input("\n\tEnter your age: "),
+        'contact_details': input("\n\tEnter your contact details: ")
+    }
+    return d
+
+
+# function to display the results
+def display_results(d):
+    print("\n\t\t\t\tUSER DETAILS")
+    print("\n")
+    for i,j in d.items():
+        print("{}:{}".format(i,j))
+
+
+# main program
+val = get_user_details()
+display_results(val)
+    
